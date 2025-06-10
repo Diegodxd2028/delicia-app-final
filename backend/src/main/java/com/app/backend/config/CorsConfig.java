79bf18264f6f2084922ff122755f11b1399f724d
Guille <eduardo.penag@pucp.edu.pe>
2025-06-09 20:25:28 -0500

    Agregado el catálogo de productos con imágenes y conexión al backend

--- backend/src/main/java/com/app/backend/config/CorsConfig.java
+++ backend/src/main/java/com/app/backend/config/CorsConfig.java
@@ -5,19 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("http://localhost:3000") // Frontend en React
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
-                        .allowedHeaders("*") // Todos los encabezados
-                        .allowCredentials(true); // Si usas cookies o autenticación
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
